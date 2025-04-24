52bdd2c26e6936f9e18b43c36fa903a9838feaf6
jaymafia <callmejaymafia@gmail.com>
2025-04-24 23:20:40 +0100

    adding contact options

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"], // Set Poppins as the default sans-serif font
+      },
+    },
+  },
+  plugins: [],
+};
