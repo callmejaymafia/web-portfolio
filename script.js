4c212f01b05869cd617edc7ebbdeb063730b352d
jaymafia <callmejaymafia@gmail.com>
2025-04-25 07:27:16 +0100

    back to top button added

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ document.addEventListener("DOMContentLoaded", () => {
     bar.style.width = width;
   });
 });
+
+const backToTopButton = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Smooth scrolling effect
+  });
+});
