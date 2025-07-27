f5682eff01462bf5976141ac6d9ca58b0765ccc3
jaymafia <callmejaymafia@gmail.com>
2025-07-27 21:39:41 +0100

    update form submission

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ backToTopButton.addEventListener("click", () => {
     behavior: "smooth", // Smooth scrolling effect
   });
 });
+
+const form = document.getElementById("my-form");
+const successMessage = document.getElementById("success-message");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault(); // prevent default form redirect
+
+  const formData = new FormData(form);
+
+  const response = await fetch(form.action, {
+    method: form.method,
+    body: formData,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  if (response.ok) {
+    form.reset(); // Clear the form
+    successMessage.classList.remove("hidden"); // Show success message
+  } else {
+    alert("Oops! There was a problem submitting your form");
+  }
+});
